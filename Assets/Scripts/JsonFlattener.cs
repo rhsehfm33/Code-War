019896a420868018afe48669d5d45f8d9c2cee67
rhsehfm33 <rhsehfm33@gmail.com>
2024-03-24 01:57:39 +0900

    feat: Improve multilingual system allowing continuous texts

--- Assets/Scripts/JsonFlattener.cs
+++ Assets/Scripts/JsonFlattener.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class JsonFlattener
 {
-    public static Dictionary<string, string> FlattenJson(string jsonText)
+    public static Dictionary<string, string> FlattenJsonText(string jsonText)
     {
         var jsonObject = JObject.Parse(jsonText);
         var flatDictionary = new Dictionary<string, string>();
@@ -12,7 +12,15 @@ public class JsonFlattener
         return flatDictionary;
     }
 
-    public static void FlattenJToken(JToken token, Dictionary<string, string> flatDictionary, string prefix)
+    public static Dictionary<string, List<string>> FlattenJsonTextArray(string jsonText)
+    {
+        var jsonObject = JObject.Parse(jsonText);
+        var flatDictionary = new Dictionary<string, List<string>>();
+        FlattenJToken(jsonObject, flatDictionary, null);
+        return flatDictionary;
+    }
+
+    private static void FlattenJToken(JToken token, Dictionary<string, string> flatDictionary, string prefix)
     {
         switch (token.Type)
         {
@@ -24,15 +32,34 @@ public class JsonFlattener
                 }
                 break;
             case JTokenType.Array:
-                int index = 0;
-                foreach (var value in token.Children())
+                break;
+            default:
+                flatDictionary[prefix] = token.ToString();
+                break;
+        }
+    }
+
+
+    private static void FlattenJToken(JToken token, Dictionary<string, List<string>> flatDictionary, string prefix)
+    {
+        switch (token.Type)
+        {
+            case JTokenType.Object:
+                foreach (var prop in token.Children<JProperty>())
                 {
-                    FlattenJToken(value, flatDictionary, $"{prefix}.{index}");
-                    index++;
+                    string propertyName = prefix != null ? $"{prefix}.{prop.Name}" : prop.Name;
+                    FlattenJToken(prop.Value, flatDictionary, propertyName);
                 }
                 break;
-            default: // For simple types, add the name-value pair to the dictionary
-                flatDictionary[prefix] = token.ToString();
+            case JTokenType.Array:
+                flatDictionary[prefix] = new List<String>();
+                foreach (var value in token.Children())
+                {
+                    if (value.Type == JTokenType.String)
+                    {
+                        flatDictionary[prefix].Add(value.ToString());
+                    }
+                }
                 break;
         }
     }
--- Assets/Scripts/LocalizationManager.cs
+++ Assets/Scripts/LocalizationManager.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 
 public class LocalizationManager : MonoBehaviour
 {
-    private Dictionary<string, string> localizedTexts;
+    private Dictionary<string, string> idToSingleText;
+    private Dictionary<string, List<String>> idToStoryTexts;
 
     public static LocalizationManager Instance { get; private set; }
 
@@ -32,7 +33,6 @@ public class LocalizationManager : MonoBehaviour
 
     private void Start()
     {
-        
         LoadLocalizationText(Language.en);
     }
 
@@ -42,12 +42,23 @@ public class LocalizationManager : MonoBehaviour
         if (localizedTextAssest != null)
         {
             string localizedTextJson = localizedTextAssest.text;
-            localizedTexts = JsonFlattener.FlattenJson(localizedTextJson);
+            idToSingleText = JsonFlattener.FlattenJsonText(localizedTextJson);
+            idToStoryTexts = JsonFlattener.FlattenJsonTextArray(localizedTextJson);
         }
 
-        foreach (KeyValuePair<string, string> kvp in localizedTexts)
+        foreach (KeyValuePair<string, string> kvp in idToSingleText)
         {
             Debug.Log($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+
+        foreach (KeyValuePair<string, List<string>> kvp in idToStoryTexts)
+        {
+            int index = 0;
+            foreach (string value in kvp.Value)
+            {
+                Debug.Log($"Key: {kvp.Key}, Index: {index} Value: {value}");
+                ++index;
+            }
         }
     }
 }
