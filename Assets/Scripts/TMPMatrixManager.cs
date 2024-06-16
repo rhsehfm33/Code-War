501fb0d3bc36579e3378a36cb1879daff99d9997
rhsehfm33 <rhsehfm33@gmail.com>
2024-06-17 00:20:13 +0900

    feat: Implement Doki

--- Assets/Scripts/TMPMatrixManager.cs
+++ Assets/Scripts/TMPMatrixManager.cs
@@ -25,12 +25,14 @@ public class TMPMatrixManager : MonoBehaviour
         yield return null;
 
         tmpMatrixInstance.transform.SetSiblingIndex(1);
+        int parentWidth = (int)transform.parent.GetComponent<RectTransform>().rect.width;
         RectTransform rectTransform = tmpMatrixInstance.GetComponent<RectTransform>();
         TMPMatrixAnimation tmpMatrixAnimation = tmpMatrixInstance.GetComponent<TMPMatrixAnimation>();
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(0f, intervalRange));
-            rectTransform.anchoredPosition = new Vector2((int)Random.Range(-17, 18) * 30, 1080);
+            rectTransform.anchoredPosition = new Vector2((int)Random.Range(-17, 18) * 30, 0);
+            rectTransform.pivot = new Vector2((float)0.5, 1);
             yield return StartCoroutine(tmpMatrixAnimation.StartMatrixAniamtion());
         }
     }
