0bde2873f9fefd4727ce377db3669cc5333365e8
rhsehfm33 <rhsehfm33@gmail.com>
2024-11-09 20:05:02 +0900

    feat Add Google Reward button

---
+++ Assets/MyGoogleAdManager.cs
@@ -0,0 +1,89 @@
+using GoogleMobileAds.Api;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGoogleAdManager : MonoBehaviour
+{
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+    private string _adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+    private string _adUnitId = "unused";
+#endif
+
+    private RewardedAd _rewardedAd;
+
+    void Start()
+    {
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            Debug.Log("Mobile Ads initialized.");
+        });
+        LoadRewardedAd();
+    }
+
+    public void LoadRewardedAd()
+    {
+        if (_rewardedAd != null)
+        {
+            _rewardedAd.Destroy();
+            _rewardedAd = null;
+        }
+
+        Debug.Log("Loading the rewarded ad.");
+
+        var adRequest = new AdRequest();
+
+        RewardedAd.Load(_adUnitId, adRequest,
+            (RewardedAd ad, LoadAdError error) =>
+            {
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("Rewarded ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Rewarded ad loaded with response : "
+                          + ad.GetResponseInfo());
+                _rewardedAd = ad;
+                RegisterReloadHandler(_rewardedAd);
+            });
+    }
+
+    public void ShowRewardedAd()
+    {
+        const string rewardMsg =
+            "Rewarded ad rewarded the user. Type: {0}, amount: {1}.";
+
+        if (_rewardedAd != null && _rewardedAd.CanShowAd())
+        {
+            _rewardedAd.Show((Reward reward) =>
+            {
+                // TODO: Reward the user.
+                Debug.Log(String.Format(rewardMsg, reward.Type, reward.Amount));
+            });
+        }
+    }
+
+    private void RegisterReloadHandler(RewardedAd ad)
+    {
+        ad.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Rewarded Ad full screen content closed.");
+
+            LoadRewardedAd();
+        };
+        ad.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Rewarded ad failed to open full screen content " +
+                           "with error : " + error);
+
+            LoadRewardedAd();
+        };
+    }
+
+}
